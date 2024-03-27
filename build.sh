ed0c2c8a5554f051675294f40642f7edbdd9098b
David Ventura <davidventura27@gmail.com>
2024-03-27 10:57:30 +0100

    tag with arch

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-docker build -t davidv27/gh-actions:latest -f Dockerfile .
-docker build -t davidv27/gh-actions-rs:latest -f Dockerfile-rs .
+docker build -t davidv27/gh-actions:$(uname -m)-latest -f Dockerfile .
+docker build -t davidv27/gh-actions-rs:$(uname -m)-latest -f Dockerfile-rs .
--- push.sh
+++ push.sh
@@ -1,2 +1,2 @@
-docker push davidv27/gh-actions:latest
-docker push davidv27/gh-actions-rs:latest
+docker push davidv27/gh-actions:$(uname -m)-latest
+docker push davidv27/gh-actions-rs:$(uname -m)-latest
