492d0bc2213372275c8055c28fc9c67afee991cd
David Ventura <davidventura27@gmail.com>
2024-03-29 14:49:05 +0100

    generate worker tokens dynamically

--- start-runner.sh
+++ start-runner.sh
@@ -3,10 +3,17 @@ if [ -z "$GH_REPOSITORY" ]; then
 	echo GH_REPOSITORY not defined
 	exit 1
 fi
-if [ -z "$GH_TOKEN" ]; then
-	echo GH_TOKEN not defined
+if [ -z "$GH_PAT" ]; then
+	echo GH_PAT not defined
 	exit 1
 fi
 . /root/.cargo/env
-./github-act-runner configure --url $GH_REPOSITORY --token $GH_TOKEN --name $(uname -m) --replace --labels $(uname -m)
+API_RESPONSE=$(curl -fsX POST -H "Authorization: token ${GH_PAT}" "https://api.github.com/repos/${GH_REPOSITORY}/actions/runners/registration-token")
+if [ $? -ne 0 ]; then
+	echo "curl failed: ${API_RESPONSE}"
+	exit 1
+fi
+
+WORKER_TOKEN=$(echo ${API_RESPONSE} | grep -oP 'token":.*?"\K.*?(?=")')
+./github-act-runner configure --url https://github.com/$GH_REPOSITORY --token $WORKER_TOKEN --name $(uname -m) --replace --labels $(uname -m)
 ./github-act-runner run
